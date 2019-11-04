d5907f522794166babc1d3ab8f1c3a908b36ce3e
koh <s1718060@ed.ac.uk>
2019-11-04 12:16:42 +0000

    initialise repository

---
+++ Combined_Installation/Combined_Installation.ino
@@ -0,0 +1,42 @@
+
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+int state = 0; //connect to Bluetooth
+//------------------------------------------------------
+MPU6050 mpu6050(Wire);
+int angleToNumber;
+float angleZ;
+int wrapTo180(int degrees)
+{
+  float radians = float(degrees) * (3.1415963 / 180.f);
+  float wrappedToPi = atan2(sin(radians), cos(radians));
+  int wrappedTo180 = (wrappedToPi * (180.f / 3.1415963f));
+  return wrappedTo180;
+}
+
+//-------------------------------------------------------
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) { // Checks whether data is comming from the serial port
+    state = Serial.read();
+  }// Reads the data from the serial port
+
+
+  mpu6050.update();
+  angleZ = mpu6050.getAngleZ();
+//  Serial.print("angleZ : ");
+  angleZ = wrapTo180(angleZ);
+//  Serial.println(angleZ);
+  angleToNumber = map(angleZ, -180, 180, 1, 9);
+  Serial.write(angleToNumber);
+  delay(1000);
+}
---
+++ Piezo_with_graph/Piezo_with_graph.ino
@@ -0,0 +1,17 @@
+const int knockSensor = A0;
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Serial.println(A0);
+
+  delay(2);
+
+
+}
